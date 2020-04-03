d2a392dc302afb0f6f3bd20ea5eb2a0e05e114ab
Shou Tseng <yakushou730@gmail.com>
2020-04-03 19:45:47 +0800

    Destroy 2 aws ec2

--- template.tf
+++ template.tf
@@ -11,27 +11,3 @@ resource "aws_subnet" "public" {
   cidr_block = "10.0.1.0/24"
   vpc_id = aws_vpc.my_vpc.id
 }
-
-resource "aws_instance" "master-instance" {
-  ami           = "ami-07f4cb4629342979c"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.id
-  tags = {
-    Name = "master-instance"
-  }
-}
-
-resource "aws_instance" "slave-instance" {
-  ami           = "ami-07f4cb4629342979c"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.id
-  tags = {
-    master_hostname = aws_instance.master-instance.private_dns
-    Name = "slave-instance"
-  }
-  depends_on = [aws_instance.master-instance]
-  lifecycle {
-    ignore_changes = [tags]
-  }
-}
-
