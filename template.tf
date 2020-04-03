2bcde338b7daee8af21d0d19f7b203e5b327637d
Shou Tseng <yakushou730@gmail.com>
2020-04-03 19:05:00 +0800

    update aws ec2 name

--- template.tf
+++ template.tf
@@ -8,6 +8,6 @@ resource "aws_instance" "hello-instance" {
   ami           = "ami-07f4cb4629342979c"
   instance_type = "t2.micro"
   tags = {
-    Name = "hello-instance"
+    Name = "hello-update-instance"
   }
 }
